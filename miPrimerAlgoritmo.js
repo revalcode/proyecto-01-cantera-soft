572e7d4a655ab7bf3c3fbb1290706e091fcedc20
revalcode <fernando.nogara@globant.com>
2022-07-24 19:31:34 -0300

    subimos otros cambios

--- miPrimerAlgoritmo.js
+++ miPrimerAlgoritmo.js
@@ -1,16 +1,23 @@
-let a;
-let b;
-let c;
+let a; //ejemplo 1
+let b; // ejemplo 2
+let c; // ejemplo 3
 
-function suma (a, b){
+function suma (a, b){ //ejemplo 4
     return c = a+b;
 }
 
 console.log(suma(3,4));
 
-if (c>9){
+if (c>9){ // ejemplo 5
     console.log("El numero es mayor a 9");
 } else {
     console.log("El numero es menor a 9");
 }
 
+for (i=0;i<10;i++){ // ejemplo 6
+    console.log(i);
+}
+
+var as;
+let ab;
+const acas='Cast';
