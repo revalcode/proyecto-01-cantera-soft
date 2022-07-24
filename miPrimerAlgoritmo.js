7666375b01a259d43cd93c6e41a0f34ffa7a532a
revalcode <fernando.nogara@globant.com>
2022-07-24 19:18:28 -0300

    subimos nuestro primer cambio

---
+++ miPrimerAlgoritmo.js
@@ -0,0 +1,16 @@
+let a;
+let b;
+let c;
+
+function suma (a, b){
+    return c = a+b;
+}
+
+console.log(suma(3,4));
+
+if (c>9){
+    console.log("El numero es mayor a 9");
+} else {
+    console.log("El numero es menor a 9");
+}
+
